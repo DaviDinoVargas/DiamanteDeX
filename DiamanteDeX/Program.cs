7f15df1cccaad10f214ef8016994d28ddf7045a2
davidinovargas <davidinovargas@gmail.com>
2025-03-25 17:22:04 -0300

    Adicionado Lógica do Diamante De X

--- DiamanteDeX/Program.cs
+++ DiamanteDeX/Program.cs
@@ -13,7 +13,20 @@
                 {
                     Console.WriteLine("Entrada inválida. insira um número ímpar.");
                 }
-
+                // lógica do jogo -> imprimir corretamente os X 
+                // após imprimir o numero correto em ordem adicionar os espaços
+                // imprimir o lado debaixo do diamante
+                // ajustar a exibição do lado inferior do diamante
+                for (int i = 1; i <= numeroEntrada; i += 2)
+                {
+                    int espacosVazios = (numeroEntrada - i) / 2;
+                    Console.WriteLine(new string(' ', espacosVazios) + new string('X', i));
+                }
+                for (int i = numeroEntrada - 2; i >= 1; i -= 2)
+                {
+                    int espacosVazios = (numeroEntrada - i) / 2;
+                    Console.WriteLine(new string(' ', espacosVazios) + new string('X', i));
+                }
                 Console.Write("\nDeseja continuar? (s/N): ");
                 string opcaoContinuar = Console.ReadLine()!.ToUpper();
                 if (opcaoContinuar != "S") break;
