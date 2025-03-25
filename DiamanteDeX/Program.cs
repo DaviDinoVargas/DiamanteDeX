81a023dfb2ff60d542953513a6b442fd3cab884c
davidinovargas <davidinovargas@gmail.com>
2025-03-25 16:10:28 -0300

    Add project files.

---
+++ DiamanteDeX/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace DiamanteDeX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Diamante de X");
+            Console.ReadLine();
+        }
+    }
+}
