135ba3d22dab5d5e9b7c9bce04e6ab6874f14d0d
davidinovargas <davidinovargas@gmail.com>
2025-03-25 16:51:29 -0300

    Implementado a entrada do usuário e a validação desta entrada.

--- DiamanteDeX/Program.cs
+++ DiamanteDeX/Program.cs
@@ -1,11 +1,21 @@
-﻿namespace DiamanteDeX
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DiamanteDeX
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            
+            int numeroEntrada;
             Console.WriteLine("Diamante de X");
-            Console.ReadLine();
+            Console.WriteLine("Digite um número ímpar: ");
+            if (!int.TryParse(Console.ReadLine(), out numeroEntrada) || numeroEntrada % 2 == 0)
+            {
+                Console.WriteLine("Entrada inválida. Por favor, insira um número ímpar.");
+            }
+
+                Console.ReadLine();
         }
     }
 }
