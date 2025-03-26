b983034bbb905b9ddb66164ea97e967e3b7258d3
davidinovargas <davidinovargas@gmail.com>
2025-03-26 00:08:40 -0300

    Implementado encapsulamento do código em funções

--- DiamanteDeX/Program.cs
+++ DiamanteDeX/Program.cs
@@ -6,34 +6,74 @@
         {
             while (true)
             {
-                int numeroEntrada;
-                Console.WriteLine("_______________________________________________");
-                Console.WriteLine("                DIAMANTE DE X");
-                Console.WriteLine("_______________________________________________");
-                Console.WriteLine("            Digite um número ímpar: ");
-                Console.WriteLine("_______________________________________________");
-                if (!int.TryParse(Console.ReadLine(), out numeroEntrada) || numeroEntrada % 2 == 0)
-                {
-                    Console.WriteLine("Entrada inválida. insira um número ímpar.");
-                }
+                ExibirMenu();
+                int numeroEntrada = ValidarEntrada();
                 // lógica do jogo -> imprimir corretamente os X 
                 // após imprimir o numero correto em ordem adicionar os espaços
                 // imprimir o lado debaixo do diamante
                 // ajustar a exibição do lado inferior do diamante
-                for (int i = 1; i <= numeroEntrada; i += 2)
+                SuperiorDiamante(numeroEntrada);
+                InferiorDiamante(numeroEntrada);
+                if (!DesejaContinuar()) break;
+
+                static void ExibirMenu()
+                {
+                    Console.Clear();
+                    Console.WriteLine("_______________________________________________");
+                    Console.WriteLine("                DIAMANTE DE X");
+                    Console.WriteLine("_______________________________________________");
+                    Console.WriteLine("            Digite um número ímpar: ");
+                    Console.WriteLine("_______________________________________________");
+                }
+
+                static int ValidarEntrada()
+                {
+                    int numeroEntrada;
+                    while (true)
+                    {
+                        if (!int.TryParse(Console.ReadLine(), out numeroEntrada) || numeroEntrada % 2 == 0)
+                        {
+                          MensagemErro();
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    return numeroEntrada;
+                }
+                static void SuperiorDiamante(int numeroEntrada)
+                {
+                    for (int i = 1; i <= numeroEntrada; i += 2)
+                    {
+                        ExibirLinhaDiamante(i, numeroEntrada);
+                    }
+                }
+                static void InferiorDiamante(int numeroEntrada)
+                {
+                    for (int i = numeroEntrada - 2; i >= 1; i -= 2)
+                    {
+                        ExibirLinhaDiamante(i, numeroEntrada);
+                    }
+                }
+                static void ExibirLinhaDiamante(int i, int numeroEntrada)
                 {
                     int espacosVazios = (numeroEntrada - i) / 2;
                     Console.WriteLine(new string(' ', espacosVazios) + new string('X', i));
                 }
-                for (int i = numeroEntrada - 2; i >= 1; i -= 2)
+                static void MensagemErro()
                 {
-                    int espacosVazios = (numeroEntrada - i) / 2;
-                    Console.WriteLine(new string(' ', espacosVazios) + new string('X', i));
+                    Console.WriteLine("_______________________________________________");
+                    Console.WriteLine("<ERRO> Entrada inválida. Insira um número ímpar.");
+                    Console.WriteLine("_______________________________________________");
                 }
-                Console.WriteLine("_______________________________________________");
-                Console.Write("\nDeseja continuar? (s/N): ");
-                string opcaoContinuar = Console.ReadLine()!.ToUpper();
-                if (opcaoContinuar != "S") break;
+
+                static bool DesejaContinuar()
+                {
+                    Console.Write("\nDeseja continuar? (s/N): ");
+                    string opcaoContinuar = Console.ReadLine()!.ToUpper();
+                    return opcaoContinuar == "S";
+                }
             }
         }
     }
