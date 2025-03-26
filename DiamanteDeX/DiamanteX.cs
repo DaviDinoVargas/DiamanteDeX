d6cfb16c3d4177b1fd96d8734d1c9ad4c554e479
davidinovargas <davidinovargas@gmail.com>
2025-03-26 08:47:08 -0300

    Implementado Classe que recebe a lógica da aplicação

---
+++ DiamanteDeX/DiamanteX.cs
@@ -0,0 +1,48 @@
+﻿namespace DiamanteDeX
+{
+    class DiamanteX
+    {
+        public static int numeroEntrada;
+
+        public static int ValidarEntrada()
+        {
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out numeroEntrada) || numeroEntrada % 2 == 0)
+                {
+                    Program.MensagemErro();
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return numeroEntrada;
+        }
+        public static void SuperiorDiamante()
+        {
+            for (int i = 1; i <= numeroEntrada; i += 2)
+            {
+                ExibirLinhaDiamante(i);
+            }
+        }
+        public static void InferiorDiamante()
+        {
+            for (int i = numeroEntrada - 2; i >= 1; i -= 2)
+            {
+                ExibirLinhaDiamante(i);
+            }
+        }
+        public static void ExibirLinhaDiamante(int i)
+        {
+            int espacosVazios = (numeroEntrada - i) / 2;
+            Console.WriteLine(new string(' ', espacosVazios) + new string('X', i));
+        }
+        public static bool DesejaContinuar()
+        {
+            Console.Write("\nDeseja continuar? (s/N): ");
+            string opcaoContinuar = Console.ReadLine()!.ToUpper();
+            return opcaoContinuar == "S";
+        }
+    }
+}
--- DiamanteDeX/Program.cs
+++ DiamanteDeX/Program.cs
@@ -7,74 +7,30 @@
             while (true)
             {
                 ExibirMenu();
-                int numeroEntrada = ValidarEntrada();
+                DiamanteX.ValidarEntrada();
                 // lógica do jogo -> imprimir corretamente os X 
                 // após imprimir o numero correto em ordem adicionar os espaços
                 // imprimir o lado debaixo do diamante
                 // ajustar a exibição do lado inferior do diamante
-                SuperiorDiamante(numeroEntrada);
-                InferiorDiamante(numeroEntrada);
-                if (!DesejaContinuar()) break;
-
-                static void ExibirMenu()
-                {
-                    Console.Clear();
-                    Console.WriteLine("_______________________________________________");
-                    Console.WriteLine("                DIAMANTE DE X");
-                    Console.WriteLine("_______________________________________________");
-                    Console.WriteLine("            Digite um número ímpar: ");
-                    Console.WriteLine("_______________________________________________");
-                }
-
-                static int ValidarEntrada()
-                {
-                    int numeroEntrada;
-                    while (true)
-                    {
-                        if (!int.TryParse(Console.ReadLine(), out numeroEntrada) || numeroEntrada % 2 == 0)
-                        {
-                          MensagemErro();
-                        }
-                        else
-                        {
-                            break;
-                        }
-                    }
-                    return numeroEntrada;
-                }
-                static void SuperiorDiamante(int numeroEntrada)
-                {
-                    for (int i = 1; i <= numeroEntrada; i += 2)
-                    {
-                        ExibirLinhaDiamante(i, numeroEntrada);
-                    }
-                }
-                static void InferiorDiamante(int numeroEntrada)
-                {
-                    for (int i = numeroEntrada - 2; i >= 1; i -= 2)
-                    {
-                        ExibirLinhaDiamante(i, numeroEntrada);
-                    }
-                }
-                static void ExibirLinhaDiamante(int i, int numeroEntrada)
-                {
-                    int espacosVazios = (numeroEntrada - i) / 2;
-                    Console.WriteLine(new string(' ', espacosVazios) + new string('X', i));
-                }
-                static void MensagemErro()
-                {
-                    Console.WriteLine("_______________________________________________");
-                    Console.WriteLine("<ERRO> Entrada inválida. Insira um número ímpar.");
-                    Console.WriteLine("_______________________________________________");
-                }
-
-                static bool DesejaContinuar()
-                {
-                    Console.Write("\nDeseja continuar? (s/N): ");
-                    string opcaoContinuar = Console.ReadLine()!.ToUpper();
-                    return opcaoContinuar == "S";
-                }
+                DiamanteX.SuperiorDiamante();
+                DiamanteX.InferiorDiamante();
+                if (!DiamanteX.DesejaContinuar()) break;  
             }
+        }
+        static void ExibirMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("_______________________________________________");
+            Console.WriteLine("                DIAMANTE DE X");
+            Console.WriteLine("_______________________________________________");
+            Console.WriteLine("            Digite um número ímpar: ");
+            Console.WriteLine("_______________________________________________");
+        }
+        public static void MensagemErro()
+        {
+            Console.WriteLine("_______________________________________________");
+            Console.WriteLine("<ERRO> Entrada inválida. Insira um número ímpar.");
+            Console.WriteLine("_______________________________________________");
         }
     }
 }
